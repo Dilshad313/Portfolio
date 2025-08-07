9e7671a24e8e16ba49669de737498b818d6ab08d
Dilshad <diluk2005786@gmail.com>
2025-08-07 22:22:11 +0530

    Project section Partially completed

---
+++ frontend/src/components/ProjectsSection.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const projects = [
+    {
+        id: 1,
+        title: "Allen Solly",
+        description: "A beautiful AllenSolly website using HTML and CSS",
+        image: "/projects/project1.png",
+        tags: ["HTML", "CSS" ],
+        demoUrl: "#",
+        githubUrl: "#",
+    },
+
+    {
+        id: 2,
+        title: "Kerala Taste",
+        description: "A beautiful Kerala Taste website using HTML and CSS",
+        image: "/projects/project2.png",
+        tags: ["HTML", "CSS" ],
+        demoUrl: "#",
+        githubUrl: "#",
+    },
+
+    {
+        id: 3,
+        title: "Star Bucks",
+        description: "A beautiful StarBucks website using HTML and CSS",
+        image: "/projects/project3.png",
+        tags: ["HTML", "CSS" ],
+        demoUrl: "#",
+        githubUrl: "#",
+    },
+
+]
+
+const ProjectsSection = () => {
+  return (
+    <section>
+
+    </section>
+  )
+}
+
+export default ProjectsSection
